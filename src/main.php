9585106d04a4b272a43a5d344fc2761fc883798f
unknown <Cedran-Bastien>
2022-10-21 13:04:45 +0200

    Ajout parametre au resultat

--- src/main.php
+++ src/main.php
@@ -13,13 +13,13 @@ echo <<<END
     <fieldset>
           <legend>Première requête : </legend>
           <bR>
-    libelle : 
+    libelle :
     <input type="text" name="libelle" id="libelle"/>
     Date début :
     <input type="date" name="DD" id="datedebut"/>
-    Date fin : 
+    Date fin :
     <input type="date" name="DF" id="datefin"/>
-    
+
     <input type="submit" value="Envoyer"/>
     <br>
     <br>
@@ -29,7 +29,7 @@ END;
 
 if(isset($_GET['libelle']) and isset($_GET['DD']) and isset($_GET['DF'])){
     $resultat = Requetes::q1($_GET['libelle'], $_GET['DD'], $_GET['DF']);
-    echo "Les véhicules disponibles pendant cette période sont : " . "<br>";
+    echo "Les véhicules disponibles({$_GET['libelle']}) de {$_GET['DD']} à {$_GET['DF']} sont : " . "<br>";
     while($data = $resultat->fetch()){
         echo $data['imm'] . " " . $data['modele'] . "<br>";
     }
@@ -48,13 +48,13 @@ echo <<<END
         <fieldset>
             <legend>Deuxième requête : </legend>
             <bR>
-    Immatriculation : 
+    Immatriculation :
     <input type="text" name="immat" id="immat"/>
     Date début :
     <input type="date" name="DD2" id="datedebut"/>
-    Date fin : 
+    Date fin :
     <input type="date" name="DF2" id="datefin"/>
-    
+
     <input type="submit" value="Envoyer"/>
     <br>
     <br>
@@ -62,7 +62,7 @@ echo <<<END
 END;
 if(isset($_POST['immat']) and isset($_POST['DD2']) and isset($_POST['DF2'])){
     $resultat1 = Requetes::q2($_POST['immat'], $_POST['DD2'], $_POST['DF2']);
-    echo "Les lignes modifiées sont : " . "<br><br>";
+    echo "Les lignes modifiées ({$_POST['immat']} entre {$_POST['DD2']} et {$_POST['DF2']} ) sont : " . "<br><br>";
     while($data = $resultat1->fetch()){
         echo $data['no_imm'] . " " . $data['datejour'] . " " . $data['paslibre'] . "<br>";
     }
@@ -79,9 +79,9 @@ echo <<<END
     <fieldset>
             <legend>Troisième requête : </legend>
         <br>
-    Nombre de jours : 
+    Nombre de jours :
     <input type="number" name="nbjours" id="nbjours"/>
-    Modèle : 
+    Modèle :
     <input type="text" name="modele" id="modele">
     <input type="submit" value="Envoyer">
     <br>
@@ -92,7 +92,7 @@ END;
 if(isset($_GET['nbjours']) and isset($_GET['modele'])){
     $resultat2 = Requetes::q3($_GET['nbjours'], $_GET['modele']);
     while($data = $resultat2->fetch()){
-        echo "Le montant de la location sera de : " .  $data['tar'] . "€<br>";
+        echo "Le montant de la location du modele {$_GET['modele']} pendant {$_GET['nbjours']} sera de : " .  $data['tar'] . "€<br>";
     }
 }
 echo "</fieldset>";
@@ -129,18 +129,19 @@ echo <<<END
     <fieldset>
         <legend>Cinquième requête : </legend>
         <br>
-        Premier modèle 
+        Premier modèle
         <input type="text" name="modele1" id="modele1"/>
         Deuxième modèle
         <input type="text" name="modele2" id ="modele2"/>
         <input type="submit" value="Envoyer"/>
         <br>
-</form>       
+</form>
 
 END;
 echo "<br>";
 if (isset($_GET['modele1']) and isset($_GET['modele2'])){
     $resultat5 = Requetes::q5($_GET['modele1'],$_GET['modele2']);
+    echo "les client ayant louer une {$_GET['modele1']} et une {$_GET['modele2']} sont : </br>";
     while($data5 = $resultat5->fetch()){
         echo $data5['n'] . " " . $data5['v'] . " " . $data5['c']  . "<br>";
     }
